fda8343384da41b79be533a3e2ae17153b50f287
Famiitry <daniel.gl97@hotmail.com>
2026-03-15 21:02:09 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Daniel Gualan",                         # Tu nombre
+    author_email="daniel.gl97@hotmail.com",                 # Tu correo electrónico
+    url="https://github.com/Lokzwery/quintoRepo",     # URL del proyecto
+)
